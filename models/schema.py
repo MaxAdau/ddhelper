c07646dee23ebad02b34cc246d301f9ef45ebc46
emax <maxadau@gmail.com>
2017-06-22 09:35:08 +0200

    Adding queries in Query Class

--- models/schema.py
+++ models/schema.py
@@ -8,6 +8,7 @@ from models.models import Event as EventModel
 from models.models import EventActor as EventActorModel
 from models.models import Actor as ActorModel
 
+from database import db_session
 
 class Event(SQLAlchemyObjectType):
     """Represents the schema of Event Model"""
@@ -15,29 +16,48 @@ class Event(SQLAlchemyObjectType):
         model = EventModel
         interfaces = (relay.Node, )
 
-
-class EventActor(SQLAlchemyObjectType):
-    """Represents the schema of EventActor Model"""
-    class Meta:
-        model = EventActorModel
-        interfaces = (relay.Node, )
-
-
+#
+# class EventActor(SQLAlchemyObjectType):
+#     """Represents the schema of EventActor Model"""
+#     class Meta:
+#         model = EventActorModel
+#         interfaces = (relay.Node, )
+#
+#
 class Actor(SQLAlchemyObjectType):
     """Represents the schema of Actor Model"""
     class Meta:
         model = ActorModel
         interfaces = (relay.Node, )
 
+
 class Query(graphene.ObjectType):
     """Todo : need to check the capabilities of this one"""
-    node = relay.Node.Field()
+#    node = relay.Node.Field()
 
-    all_event_actor = SQLAlchemyConnectionField(EventActor)
-    all_actors = SQLAlchemyConnectionField(Actor)
-    all_events = SQLAlchemyConnectionField(Event)
+    #all_actors = SQLAlchemyConnectionField(Actor)
+    #all_events = SQLAlchemyConnectionField(Event)
 
-    actor_ = graphene.Field(Actor)
+#    actor_ = graphene.Field(Actor)
+
+    event = graphene.Field(Event)
+    events = graphene.List(Event)
+
+    actor = graphene.Field(Actor)
+    actors = graphene.List(Actor)
+
+    def resolve_event(self, *args, **kwargs):
+        return db_session.query(EventModel).first()
+
+    def resolve_events(self, *args, **kwargs):
+        return db_session.query(EventModel).all()
+
+    def resolve_actor(self, *args, **kwargs):
+        return db_session.query(ActorModel).first()
+
+    def resolve_actors(self, *args, **kwargs):
+        return db_session.query(ActorModel).all()
 
 # Building my schema object
-schema = graphene.Schema(query=Query, types=[Event, EventActor, Actor])
+#schema = graphene.Schema(query=Query, types=[Event, EventActor, Actor])
+schema = graphene.Schema(query=Query, types=[Event, Actor])
